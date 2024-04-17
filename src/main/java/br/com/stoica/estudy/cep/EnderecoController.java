0b667cfe5f3dd9b39ea02e129188a3a0200e4ecb
LcnFraga27 <luciano.lucas@bllc.com.br>
2024-04-16 22:33:32 -0300

    ENDERECO
    controller

--- src/main/java/br/com/stoica/estudy/cep/EnderecoController.java
+++ src/main/java/br/com/stoica/estudy/cep/EnderecoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/endereco")
@@ -13,8 +15,25 @@ public class EnderecoController {
     @Autowired
     private ViaCepClient viaCepClient;
 
+    @Autowired
+    private PessoaEnderecoRepository rRepository;
+
+    @Autowired
+    private EnderecoRepository eRepository;
+
     @GetMapping("/{cep}")
     public EnderecoDTO getEndereco(@PathVariable String cep) {
         return viaCepClient.getEndereco(cep); 
     }
+
+    @GetMapping("/relacionamentos")
+    public List<?> getRelacionamentos() {
+        return rRepository.findAll();
+    }
+    
+    @GetMapping("/enderecos")
+    public List<?> getEnderecos() {
+        return eRepository.findAll();
+    }
+
 }
